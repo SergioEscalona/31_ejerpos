c521f9654ad475a83e489d41ccf8fcb43e9b65de
SergioEscalona <sergio.escalona.alvarez@gmail.com>
2017-04-06 18:50:49 +0200

    skr

--- app/src/main/java/ejercicio31/a31_ejerpos/MainActivity.java
+++ app/src/main/java/ejercicio31/a31_ejerpos/MainActivity.java
@@ -7,13 +7,19 @@ import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import javabean.Posicion;
 import modelo.GestionBD;
@@ -38,13 +44,20 @@ public class MainActivity extends AppCompatActivity {
         }
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 10, new CambioLoc());
         }
+
+        public void verLoc (View v){
+            lstPosiciones=(EditText)this.findViewById(R.id.lstLocal);
+            Comunicacion com=new Comunicacion();
+            com.execute(lstPosiciones);
+        }
     }
+
 
 class CambioLoc implements LocationListener{
     @Override
     public void onLocationChanged(Location location) {
         Posicion pd=new Posicion(location.getLatitude()+"",location.getLongitude()+"");
-        GestionBD gpos=new GestionBD(CambioLoc.this,"bd");
+        GestionBD gpos=new GestionBD(this,"bd");
         gpos.altaPosicion(pd);
 
     }
